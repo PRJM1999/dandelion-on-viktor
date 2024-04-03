01b8e82861cdf518aa8608989b0f9c1812110068
Patrick <patrick.montgomery@atkinsglobal.com>
2024-04-03 15:09:03 +0100

    Initial Commit

---
+++ speckle_integration.py
@@ -0,0 +1,21 @@
+from specklepy.api.client import SpeckleClient
+from specklepy.api.credentials import get_default_account
+
+class SpeckleIntegration:
+    def __init__(self):
+        self.client = self.initialize_speckle_client()
+
+    def initialize_speckle_client(self, ):
+        """Initializes and returns a Speckle client."""
+        client = SpeckleClient(host="https://speckle.uksouth.cloudapp.azure.com")
+        account = get_default_account()
+        if account:
+            client.authenticate(token='0d82bd910c25045fbbf85c4ffe9095ac495e671e0a')
+        return client
+
+    def get_projects(self):
+        """Fetches projects from Speckle and returns them."""
+        projects = self.client.stream.list()  # Adjust based on what you want to fetch
+        return projects
+
+print(SpeckleIntegration().get_projects())
