813f0581925882ca6a1d5513008e256f8b0d18a5
Patrick <patrick.montgomery@atkinsglobal.com>
2024-04-03 15:09:03 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from viktor import ViktorController
+from viktor.parametrization import ViktorParametrization, Text, OptionField
+from speckle_integration import SpeckleIntegration
+import json
+
+projects = SpeckleIntegration().get_projects()
+print(projects)
+
+class Parametrization(ViktorParametrization):
+    projects_json = Text(str(projects[0].name))
+    project_select = OptionField('Available Options', options=[str(projects[0].name), str(projects[2].name)])
+
+
+class Controller(ViktorController):
+    label = 'My Entity Type'
+    parametrization = Parametrization
+
+
+
+
+
---
+++ speckle_integration.py
@@ -0,0 +1,21 @@
+from specklepy.api.client import SpeckleClient
+from specklepy.api.credentials import get_default_account
+
+class SpeckleIntegration:
+    def __init__(self):
+        self.client = self.initialize_speckle_client()
+
+    def initialize_speckle_client(self, ):
+        """Initializes and returns a Speckle client."""
+        client = SpeckleClient(host="***REMOVED***")
+        account = get_default_account()
+        if account:
+            client.authenticate(token='***REMOVED***')
+        return client
+
+    def get_projects(self):
+        """Fetches projects from Speckle and returns them."""
+        projects = self.client.stream.list()  # Adjust based on what you want to fetch
+        return projects
+
+print(SpeckleIntegration().get_projects())
