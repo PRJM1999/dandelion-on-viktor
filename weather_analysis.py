276a839938bc9869e5dc0d78c9dbd166b688ac1c
Patrick <patrick.montgomery@atkinsglobal.com>
2024-04-10 16:40:19 +0100

    Moved categories into seperate json file

--- weather_analysis.py
+++ weather_analysis.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from weather_data import WeatherData
 import utci_optimised
 import numpy as np
+import json
 
 
 class WeatherAnalysis(ABC):
@@ -21,22 +22,45 @@ class WeatherAnalysis(ABC):
         """
         pass
 
-
 class UTCICalculator(WeatherAnalysis):
+    def __init__(self, weather_data):
+        super().__init__(weather_data)
+        self.categories = self._load_categories("categories.json")
+    
+    @staticmethod
+    def _load_categories(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
 
-    def calculate(self):
-        """
-        Calculates the Universal Thermal Climate Index (UTCI) based on the provided WeatherData.
-        """
+    def calculate(self, return_stress_category=False, return_comfort_rating=False):
         eh_pa = self._exponential(self.weather_data.dry_bulb_temp) * (self.weather_data.relative_humidity / 100.0)
         delta_t_tr = self.weather_data.radiant_temp - self.weather_data.dry_bulb_temp
-        pa = eh_pa / 10.0  # Convert vapour pressure to kPa
+        pa = eh_pa / 10.0
         
         utci_approx = utci_optimised.calc(self.weather_data.dry_bulb_temp, self.weather_data.wind_speed, delta_t_tr, pa)
         
         output = {'utci': np.round_(utci_approx, 5).tolist()}
 
+        if return_stress_category:
+            output['stress_category'] = self._get_category(utci_approx, self.categories['STRESS_CATEGORIES'])
+        
+        if return_comfort_rating:
+            output['comfort_rating'] = self._get_category(utci_approx, self.categories['COMFORT_RATINGS'])
+
         return output
+
+
+    def _get_category(self, utci_value, category_dict):
+        for key, category in category_dict.items():
+            # Split the key into lower and upper bounds, then handle 'null' appropriately
+            bounds = key.split(', ')
+            lower = float(bounds[0]) if bounds[0] != "null" else None
+            upper = float(bounds[1]) if bounds[1] != "null" else None
+
+            if (lower is None or utci_value > lower) and (upper is None or utci_value <= upper):
+                return category
+        return "unknown"
+
 
     def _exponential(self, t_db):
         g = [
@@ -54,3 +78,9 @@ class UTCICalculator(WeatherAnalysis):
             es = es + (i * np.power(tk, count - 2))
         es = np.exp(es) * 0.01  # convert Pa to hPa
         return es
+
+weather_data = WeatherData(dry_bulb_temp=25, radiant_temp=30, wind_speed=1.5, relative_humidity=50, units="SI")
+calculator = UTCICalculator(weather_data=weather_data)
+result = calculator.calculate(return_stress_category=True, return_comfort_rating=True)
+
+print(result)
