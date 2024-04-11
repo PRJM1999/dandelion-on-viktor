69a0d6d87ca8c90c58b14edf21930dc0b515ecfa
Patrick <patrick.montgomery@atkinsglobal.com>
2024-04-11 17:42:11 +0100

    Updated unit tests in response to folder structure change

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from pathlib import Path
 # Assuming conftest.py is located directly inside the tests folder,
 # and we want to add the src folder to sys.path
 root_dir = Path(__file__).parent.parent
-src_dir = root_dir
+print(root_dir)
+src_dir = root_dir / 'src'
 
 sys.path.insert(0, str(src_dir))
