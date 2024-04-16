d9b6ae0c25ba7ab9ece013fbc08e8cd3ee784ba1
Patrick <patrick.montgomery@atkinsglobal.com>
2024-04-16 23:23:36 +0100

    modified weather analysis

--- src/weather_analysis.py
+++ src/weather_analysis.py
@@ -1,30 +1,15 @@
 from abc import ABC, abstractmethod
 from data_objects import WeatherData
 from utility import utci_optimised
+from data_objects import WeatherData
 import numpy as np
 import json
 
 
-class WeatherAnalysis(ABC):
-    """
-    Weather Analysis abstract class
-    """
+class UTCICalculator():
 
     def __init__(self, weather_data: WeatherData):
         self.weather_data = weather_data
-    
-    @abstractmethod
-    def calculate(self):
-        """
-        Perform the specific weather analysis calculation.
-        
-        Must be implemented by subclasses.
-        """
-        pass
-
-class UTCICalculator(WeatherAnalysis):
-    def __init__(self, weather_data):
-        super().__init__(weather_data)
         self.categories = self._load_categories("categories.json")
     
     @staticmethod
@@ -33,9 +18,7 @@ class UTCICalculator(WeatherAnalysis):
             return json.load(file)
 
     def calculate(self):
-        eh_pa = self._exponential(self.weather_data.dry_bulb_temp) * (self.weather_data.relative_humidity / 100.0)
-        delta_t_tr = self.weather_data.radiant_temp - self.weather_data.dry_bulb_temp
-        pa = eh_pa / 10.0
+        eh_pa, delta_t_tr, pa = self._calculate_environmental_factors()
         
         utci_approx = utci_optimised.calc(self.weather_data.dry_bulb_temp, self.weather_data.wind_speed, delta_t_tr, pa)
         
@@ -46,26 +29,20 @@ class UTCICalculator(WeatherAnalysis):
 
         return output
 
-    def _get_category(self, utci_value: float, category_dict: dict) -> str:
-        """
-        Determines the category of the given UTCI value based on predefined bounds.
-
-        Parameters:
-        - utci_value (float): The Universal Thermal Climate Index value to categorize.
-        - category_dict (dict): A dictionary mapping string bounds to category labels.
+    def _calculate_environmental_factors(self):
+        """"
+        Calculates the environmental factors needed for UTCI calculation.
 
         Returns:
-        - str: The category label if a matching range is found; "unknown" otherwise.
-
+        - eh_pa (float): Partial vapor pressure in hPa.
+        - delta_t_tr (float): Difference between radiant temperature and dry bulb temperature.
+        - pa (float): Modified partial vapor pressure used in UTCI calculation.
         """
-        for key, category in category_dict.items():
-            bounds = key.split(', ')
-            lower = float(bounds[0]) if bounds[0] != "null" else None
-            upper = float(bounds[1]) if bounds[1] != "null" else None
+        eh_pa = self._exponential(self.weather_data.dry_bulb_temp) * (self.weather_data.relative_humidity / 100.0)
+        delta_t_tr = self.weather_data.radiant_temp - self.weather_data.dry_bulb_temp
+        pa = eh_pa / 10.0
+        return eh_pa, delta_t_tr, pa
 
-            if (lower is None or utci_value > lower) and (upper is None or utci_value <= upper):
-                return category
-        return "unknown"
 
     def _exponential(self, t_db: float) -> float:
         """
@@ -95,6 +72,10 @@ class UTCICalculator(WeatherAnalysis):
             es = es + (i * np.power(tk, count - 2))
         es = np.exp(es) * 0.01  # convert Pa to hPa
         return es
+
+
+
+
 
 weather_data = WeatherData(dry_bulb_temp=25, radiant_temp=30, wind_speed=1.5, relative_humidity=50, units="SI")
 calculator = UTCICalculator(weather_data=weather_data)
